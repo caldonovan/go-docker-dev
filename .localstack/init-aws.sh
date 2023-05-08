4bb53b13df38294e39b4d8da0daab635fd7091fe
Cal <caldonovan@outlook.com>
2023-05-08 13:37:03 +0100

    refactor localstack init script

--- .localstack/init-aws.sh
+++ .localstack/init-aws.sh
@@ -2,35 +2,10 @@
 
 set -euo pipefail
 
-echo "configuring aws infrastructure"
-echo "==================="
 export LOCALSTACK_HOST=localhost
 export AWS_DEFAULT_REGION=eu-central-1
 export AWS_ACCESS_KEY_ID=12345678
 export AWS_SECRET_ACCESS_KEY=12345678
 
-
-create_queue() {
-    local QUEUE_NAME_TO_CREATE=$1
-    aws --endpoint-url=http://${LOCALSTACK_HOST}:4566 sqs create-queue --queue-name ${QUEUE_NAME_TO_CREATE} --attributes VisibilityTimeout=30
-}
-
-create_bucket() {
-    local BUCKET_NAME_TO_CREATE=$1
-    aws --endpoint-url=http://${LOCALSTACK_HOST}:4566 s3 mb s3://${BUCKET_NAME_TO_CREATE}
-}
-
-# create queues
-for queue in "queue1"
-do
-  create_queue $queue
-done
-
-
-# create buckets
-for bucket in "bucket1"
-do
-  create_bucket $bucket
-done
-
-echo "finished running localstack bootstrap"
+aws --endpoint-url=http://${LOCALSTACK_HOST}:4566 sqs create-queue --queue-name queue1 --attributes VisibilityTimeout=30
+aws --endpoint-url=http://${LOCALSTACK_HOST}:4566 s3 mb s3://bucket1
